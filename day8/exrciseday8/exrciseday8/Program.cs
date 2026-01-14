2b928b684fd1644b82321b3075e433870c6ce5b7
Kritika Sharma <kritikasharma0514@gmail.com>
2026-01-14 14:34:32 +0530

    day 8 exercise

---
+++ day8/exrciseday8/exrciseday8/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace exrciseday8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string[] arr = input.Split('|');
+            string userName = arr[0];
+            string msg = arr[1];
+
+            string cleanMessage = msg.Trim().ToLower();
+
+            bool containsSuccessful = cleanMessage.Contains("successful");
+
+            string standardMessage = "login successful";
+
+            string status;
+
+            if (!containsSuccessful)
+            {
+                status = "LOGIN FAILED";
+            }
+            else if (cleanMessage.Equals(standardMessage))
+            {
+                status = "LOGIN SUCCESS";
+            }
+            else
+            {
+                status = "LOGIN SUCCESS (CUSTOM MESSAGE)";
+            }
+            Console.WriteLine($"User     : {userName}");
+            Console.WriteLine($"Message  : {cleanMessage}");
+            Console.WriteLine($"Status   : {status}");
+        }
+    }
+}
