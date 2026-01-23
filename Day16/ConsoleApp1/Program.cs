73e8134a0cc5d19968d5cb1b1cdf904ee9c023fe
Kritika Sharma <kritikasharma0514@gmail.com>
2026-01-23 12:28:14 +0530

    exercise day 16 uploaded

---
+++ Day16/ConsoleApp1/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace ConsoleApp1
+{
+
+    class ApplicationConfig
+    {
+       
+        public static string ApplicationName { get; set; }
+        static string Environment { get; set; }
+
+        public static int AccessCount { get; set; }
+
+        public static bool IsInitialised { get; set; }
+
+        static ApplicationConfig()
+        {
+        
+            ApplicationName = "MyApp";
+            Environment = "Development";
+            AccessCount = 0;
+            IsInitialised = true;
+            Console.WriteLine("Static constructor executed");
+
+                
+        }
+        public static void Initialize(string appName, string env)
+        {
+            ApplicationName = appName;
+            Environment = env;
+            IsInitialised = true;
+            AccessCount++;
+        }
+
+        public static string GetConfigurationSummary()
+        {
+            AccessCount++;
+            return $"app name : {ApplicationName}\n" +
+                $"environment : {Environment}\n" +
+                $"access count : {AccessCount}\n" +
+                $"initialisation status :  {IsInitialised}\n";
+        }
+        public static void ResetConfiguration()
+        {
+            ApplicationName = "MyApp";
+            Environment = "Development";
+            IsInitialised = false;
+            AccessCount++;
+        }
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ApplicationConfig.ApplicationName = "myyapp";
+            ApplicationConfig.Initialize("hellooapp", "env");
+            Console.WriteLine(ApplicationConfig.GetConfigurationSummary());
+               
+            ApplicationConfig.ResetConfiguration();
+            Console.WriteLine("----------------reset to default--------------------");
+            Console.WriteLine(ApplicationConfig.GetConfigurationSummary());
+
+
+
+        }
+    }
+}
