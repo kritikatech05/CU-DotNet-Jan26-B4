e8e098ec496d15ab2c4413f60787ea95d8a3e697
Kritika Sharma <kritikasharma0514@gmail.com>
2026-03-17 09:53:26 +0530

    updated

---
+++ Daily Exercises/Day56-01-MVC CRUD/Controllers/LoanController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuickLoan.Models;
+
+namespace QuickLoan.Controllers
+{
+    public class LoanController : Controller
+    {
+        private static List<Loan> loans = new List<Loan>();
+
+        public IActionResult Index()
+        {
+            return View(loans);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Loan loan)
+        {
+            if (ModelState.IsValid)
+            {
+                loan.Id = loans.Count + 1;
+                loans.Add(loan);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(loan);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var loan = loans.FirstOrDefault(x => x.Id == id);
+
+            if (loan == null)
+                return NotFound();
+
+            return View(loan);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Loan loan)
+        {
+            if (ModelState.IsValid)
+            {
+                var existingLoan = loans.FirstOrDefault(x => x.Id == loan.Id);
+
+                if (existingLoan != null)
+                {
+                    existingLoan.BorrowerName = loan.BorrowerName;
+                    existingLoan.LenderName = loan.LenderName;
+                    existingLoan.Amount = loan.Amount;
+                    existingLoan.IsSettled = loan.IsSettled;
+                }
+
+                return RedirectToAction("Index");
+            }
+
+            return View(loan);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var loan = loans.FirstOrDefault(x => x.Id == id);
+
+            if (loan != null)
+                loans.Remove(loan);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
